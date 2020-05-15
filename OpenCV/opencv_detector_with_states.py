56c6b2724462246ce7e5991a4540761ad67a7f3d
lspetersen <trezum@gmail.com>
2020-05-15 19:43:08 +0200

    Working state change

--- OpenCV/opencv_detector_with_states.py
+++ OpenCV/opencv_detector_with_states.py
@@ -6,69 +6,85 @@ import time
 # Assigning our static_back to None
 static_back = None
 
-# List when any moving object appear
-motion_list = [None, None]
+# -1 = quit
+#  0 = motion detection
+#  1 = pedestrian detection
+#  2 = face detection
+state = 0
+
 start = time.time()
 
 # Capturing video
 video = cv2.VideoCapture(0)
 
 # Infinite while loop to treat stack of image as video
-while True:
-    # Reading frame(image) from video
-    check, frame = video.read()
+def motion_detection(static_back, start):
+    # Infinite while loop to treat stack of image as video
+    while True:
+        # Reading frame(image) from video
+        check, frame = video.read()
 
-    # Initializing motion = 0(no motion)
-    motion = 0
+        # Initializing motion = 0(no motion)
+        motion = 0
 
-    # Converting color image to gray_scale image
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        # Converting color image to gray_scale image
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-    # Converting gray scale image to GaussianBlur
-    # so that change can be find easily
-    gray = cv2.GaussianBlur(gray, (21, 21), 0)
+        # Converting gray scale image to GaussianBlur
+        # so that change can be find easily
+        gray = cv2.GaussianBlur(gray, (21, 21), 0)
 
-    # In first iteration we assign the value
-    # of static_back to our first frame
-    if static_back is None:
-        static_back = gray
-        continue
-
-    if (abs( start - time.time())>2):
-        static_back = gray
-        start = time.time()
-
-    # Difference between static background
-    # and current frame(which is GaussianBlur)
-    diff_frame = cv2.absdiff(static_back, gray)
-
-    # If change in between static background and
-    # current frame is greater than 30 it will show white color(255)
-    thresh_frame = cv2.threshold(diff_frame, 30, 255, cv2.THRESH_BINARY)[1]
-    thresh_frame = cv2.dilate(thresh_frame, None, iterations=2)
-
-    # Finding contour of moving object
-    (image, cnts, hier) = cv2.findContours(thresh_frame.copy(),
-                                    cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-
-    for contour in cnts:
-        if cv2.contourArea(contour) < 1000:
+        # In first iteration we assign the value
+        # of static_back to our first frame
+        if static_back is None:
+            static_back = gray
             continue
 
-        rect = cv2.minAreaRect(contour)
-        box = cv2.boxPoints(rect)
-        box = numpy.int0(box)
-        cv2.drawContours(frame, [box], 0, (0, 0, 255), 2)
+        if (abs(start - time.time()) > 2):
+            static_back = gray
+            start = time.time()
 
-    # Displaying the frame
-    cv2.imshow("Detector with states", frame)
+        # Difference between static background
+        # and current frame(which is GaussianBlur)
+        diff_frame = cv2.absdiff(static_back, gray)
 
-    key = cv2.waitKey(1)
-    # if q entered whole process will stop
-    if key == ord('q'):
+        # If change in between static background and
+        # current frame is greater than 30 it will show white color(255)
+        thresh_frame = cv2.threshold(diff_frame, 30, 255, cv2.THRESH_BINARY)[1]
+        thresh_frame = cv2.dilate(thresh_frame, None, iterations=2)
+
+        # Finding contour of moving object
+        (image, cnts, hier) = cv2.findContours(thresh_frame.copy(),
+                                               cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+        for contour in cnts:
+            if cv2.contourArea(contour) < 1000:
+                continue
+
+            rect = cv2.minAreaRect(contour)
+            box = cv2.boxPoints(rect)
+            box = numpy.int0(box)
+            cv2.drawContours(frame, [box], 0, (0, 0, 255), 2)
+
+        # Displaying the frame
+        cv2.imshow("Detector with states", frame)
+
+        key = cv2.waitKey(1)
+        # if q entered whole process will stop
+        if key == ord('q'):
+            return -1
+        # if n entered change state to
+        if key == ord('n'):
+            return 1
+
+
+while True:
+    if state == -1:
         break
-    if key == ord('n'):
-        break
+    if state == 0:
+        state = motion_detection(static_back, start)
+
+
 video.release()
 
 # Destroying all the windows
